42c516c74d176e60604314b74816eda6d346f1d8
Edwin García <spark.com.co@gmail.com>
2020-09-27 18:56:29 -0500

    [feature] Server listening

---
+++ src/config/index.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+
+const isDev = process.env.NODE_ENV !== 'production'
+
+if (isDev){
+  dotenv.config({ path: './.env.dev' })
+}else{
+  dotenv.config()
+}
+
+const config = {
+    isDev,
+    port: process.env.PORT || 3000
+}
+
+module.exports = config
