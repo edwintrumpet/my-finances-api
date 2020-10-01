a8da4b7e0de19e76a2cbf3c915660b470193c94e
Edwin García <spark.com.co@gmail.com>
2020-10-01 12:14:26 -0500

    [feature] Handle errors and not found request

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ const debug = require('debug')('app:server');
 
 const { port } = require('./config/index');
 const routes = require('./routes');
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middlewares/errorHandlers');
+const notFoundHandler = require('./utils/middlewares/notFoundHandler');
 
 // Initializations
 const app = express();
 
+// Middlewares
+app.use(express.json());
+
 // Routes
 routes(app);
+
+// Handle Errors
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
+
+// Catch 404
+app.use(notFoundHandler);
 
 // Server
 app.listen(port, (err) => {
